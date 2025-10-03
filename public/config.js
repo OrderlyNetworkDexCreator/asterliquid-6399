514a562db517c8c1df34de1500bb365cb0148a08
Mario Reder <mario.reder@pm.me>
2025-10-03 23:45:41 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -18,7 +18,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_HAS_PRIMARY_LOGO": "true",
   "VITE_HAS_SECONDARY_LOGO": "true",
   "VITE_ENABLED_MENUS": "Trading,Portfolio,Markets,Vaults,Rewards,Leaderboard",
-  "VITE_CUSTOM_MENUS": "About, https://asterliquid.org/; Telegram, https://t.me/asterliquid",
+  "VITE_CUSTOM_MENUS": "About, https://asterliquid.org/; Telegram, https://t.me/asterliquid; X, https://x.com/AsterLiquidDEX",
   "VITE_ENABLE_CAMPAIGNS": "false",
   "VITE_TELEGRAM_URL": "https://t.me/asterliquid",
   "VITE_DISCORD_URL": "",
